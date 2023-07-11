c7d9aa46acac3c1c2fdcc674ecc358aa85f3ebbc
MohamedAhmed12 <mohamed7el17@gmail.com>
2023-07-11 14:44:16 +0300

    217-contains-duplicate in PHP

---
+++ php/217- contains-duplicate.php
@@ -0,0 +1,11 @@
+class Solution {
+    /**
+    * @param Integer[] $nums
+    * @return Boolean
+    */
+    function containsDuplicate($nums) {
+        $set = array_unique($nums);
+
+        return count($set) == count($nums) ? false : true;
+    }
+}
